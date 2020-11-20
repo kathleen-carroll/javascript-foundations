33266b672b121622e90e8fe873c91c918ad402c5
Kathleen Carroll <khakicarroll@comcast.net>
2020-11-20 16:49:53 -0700

    Complete unicorn class and pass all tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,16 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = 'white') {
     this.name = name;
     this.color = color;
   }
+
+  isWhite() {
+    return this.color === 'white'
+  };
+
+  says(words) {
+    return `**;* ${words} *;**`
+  };
 }
 
 module.exports = Unicorn;

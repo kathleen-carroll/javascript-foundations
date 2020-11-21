ff907f1bca1f61be566702c5e9a73a7679770f7d
Kathleen Carroll <khakicarroll@comcast.net>
2020-11-20 17:06:42 -0700

    Finish dragon and pass all tests

---
+++ mythical-creatures/exercises/dragon.js
@@ -0,0 +1,19 @@
+class Dragon {
+  constructor (name, rider, color) {
+    this.name = name
+    this.rider = rider
+    this.color = color
+    this.hungry = true
+    this.count = 0
+  }
+
+  eat() {
+    this.count += 1
+
+    if (this.count == 3) {
+      this.hungry = false
+    }
+  }
+}
+
+module.exports = Dragon;
